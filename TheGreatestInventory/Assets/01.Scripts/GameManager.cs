a5771efc78d5bbfb2e9931a9a96b72801fcbfdf9
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-06-11 02:30:05 +0900

    [Feat] Addressable로 아이템 불러오기 기능
    기존 : GameManager의 List<ItemData> itemDatas; 필드는 인스펙터에서 직접 추가해줬음
    
    변경 : Addressable을 활용하여 인스펙터에 추가하지 않아도 자동으로 로드

--- TheGreatestInventory/Assets/01.Scripts/GameManager.cs
+++ TheGreatestInventory/Assets/01.Scripts/GameManager.cs
@@ -1,15 +1,16 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using System.Threading.Tasks;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    [field:SerializeField] public Character player { get; private set; }
+    [field: SerializeField] public Character player { get; private set; }
 
     public List<ItemData> itemDatas;
-    
+
     private void Awake()
     {
         if (instance == null)
@@ -21,22 +22,41 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        
     }
 
-    private void Start()
+    private async void Start()
     {
+        // Addressable 비동기 로드 대기
+        await LoadItemDatas();
+
+        // 로드가 완료되면 기존 SetData() 호출
         SetData();
+    }
+
+    private async Task LoadItemDatas()
+    {
+        var handle = Addressables.LoadAssetsAsync<ItemData>("Sword", null);
+        await handle.Task;
+
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            itemDatas = new List<ItemData>(handle.Result);
+            Debug.Log($"Addressable에서 아이템 데이터 {itemDatas.Count}개 로드 완료");
+        }
+        else
+        {
+            Debug.LogError("아이템 데이터 로드 실패");
+            itemDatas = new List<ItemData>();
+        }
     }
 
     private void SetData()
     {
+        // 기존 로직 유지
         player = new Character("낭만용사", 1, "얼큰오이티라미수 \nvs \n제티마라탕", 10, 100, 5, 5, 100, 10, itemDatas);
-        
+
         UIManager.instance.MainMenu.SetUIMainMenu(player);
         UIManager.instance.Status.SetUIStatus(player);
         UIManager.instance.Inventory.SetInventory(player);
     }
-    
-    
 }
